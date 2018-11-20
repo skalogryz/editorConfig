7e7c44876fbfac40bb18913d03161627f69d20f7
Dmitry Boyarintsev <skalogryz.lists@gmail.com>
2018-11-20 16:35:33 -0500

    adding for curvy brakets support for EC

--- edconfglob.pas
+++ edconfglob.pas
@@ -8,6 +8,7 @@ interface
 function FileNameMatch(const pat, s: string): Boolean;
 
 // editorConfig match, as desribed: https://editorconfig.org/#supported-properties
+// todo: handle characters escape
 function ECMatch(const pat, s: string): Boolean;
 
 implementation
@@ -81,7 +82,52 @@ begin
   Result := _FileNameMatch(pat, 1, s, 1);
 end;
 
-function _ECMatch(const p: string; pidx: integer; const s: string; sidx: integer): Boolean;
+function TryStrToInt(const s: string; out v: Int64): Boolean;
+var
+  err : integer;
+begin
+  Result := (s<>'')
+    and (s[1] in ['-','+','0'..'9'])
+    and ((length(s)>1) or (s[1] in ['0'..'9']));
+  if not Result then Exit;
+  Val(s, v, err);
+  Result := err = 0;
+end;
+
+type
+  TECStrings = record
+    offset, len : integer;
+  end;
+
+  { TECSet }
+
+  TECSet = class(TObject)
+  public
+    Pattern : string;
+    FullIdx : integer;
+    FullLen : integer;
+
+    isNum   : Boolean;
+    min,max : Int64;
+    Count   : integer;
+    Strs    : array of TECStrings;
+    procedure Parse(const p: string; var pidx: integer);
+    function Match(const s: string; var sidx: Integer): Boolean;
+    procedure AddWord(aidx, alen: integer);
+  end;
+
+  { TECContext }
+
+  TECContext = class
+  private
+    st : array of TECSet;
+    count: integer;
+  public
+    destructor Destroy; override;
+    function GetSet(const p: string; var pidx: integer): TECSet;
+  end;
+
+function _ECMatch(ctx: TECContext; const p: string; pidx: integer; const s: string; sidx: integer): Boolean;
 var
   pi, i, j : integer;
   stoppath : Boolean;
@@ -96,6 +142,7 @@ begin
         inc(i);
       end;
       '[': Result := _ParseSet(p, pi, s, i);
+      '{': Result := ctx.GetSet(p, pi).Match(s,i);
       '*': begin
         inc(pi);
         stoppath := true;
@@ -123,7 +170,7 @@ begin
               i:=j;
               break;
             end else begin
-              Result := _ECMatch(p, pi, s, j);
+              Result := _ECMatch(ctx, p, pi, s, j);
               if Result then Exit;
             end;
       end;
@@ -139,8 +186,136 @@ begin
 end;
 
 function ECMatch(const pat, s: string): Boolean;
+var
+  ctx: TECContext;
 begin
-  Result := _ECMatch(pat, 1, s, 1);
+  ctx:=TECContext.Create;
+  try
+    Result := _ECMatch(ctx, pat, 1, s, 1);
+  finally
+    ctx.Free;
+  end;
+end;
+
+procedure TECSet.Parse(const p: string; var pidx: integer);
+var
+  j: integer;
+  k: integer;
+
+  knownum: Boolean;
+  n1,n2: Int64;
+  pstart: Integer;
+begin
+  Pattern := p;
+  FullIdx:=pidx;
+  if (p[pidx] = '{') then inc(pidx);
+  j:=pidx;
+  knownum:=false;
+  pstart:=pidx;
+
+  while (pidx <= length(p)) and (p[pidx]<>'}') do begin
+
+    if (p[pidx]=',') then begin
+      AddWord(j, pidx-j);
+      j:=pidx+1;
+      knownum := true; // this is a list of words, and not numbers range
+      inc(pidx);
+
+    end else if (p[pidx]='.') and not knownum and (pidx<length(p)) and (p[pidx+1]='.') then begin
+      knownum:=true;
+      if TryStrToInt( Copy(p, j, pidx-j), n1) then begin
+        j:=pidx+2;
+        k:=j;
+        while (k<=length(p)) and (p[k]<>'}') do inc(k);
+        isNum := TryStrToInt( Copy(p, j, k-j), n2);
+        pidx:=k;
+        if isNum then begin
+          min:=n1;
+          max:=n2;
+        end;
+      end;
+      if not isNum then inc(pidx);
+    end else
+      inc(pidx);
+  end;
+
+  if not isNum then AddWord(j, pidx-j);
+
+  inc(pidx);
+  FullLen := pidx - pstart;
+end;
+
+function TECSet.Match(const s: string; var sidx: Integer): Boolean;
+var
+  i,j : integer;
+  leftlen : integer;
+  v: Int64;
+  err: integer;
+begin
+  if not isNum then begin
+    leftlen := length(s) - sidx + 1;
+    Result := false;
+    for i:=0 to Count-1 do
+      if Strs[i].len<=leftlen then begin
+        if CompareChar( s[sidx], Pattern[strs[i].offset], Strs[i].len)=0 then begin
+          Result := true;
+          inc(sidx, Strs[i].len);
+          Exit;
+        end;
+      end;
+  end else begin
+    Result := sidx<=length(s);
+    if not Result then Exit;
+    i:=sidx;
+    j:=i;
+    if (s[i] in ['+','-']) then inc(i);
+    while (i<=length(s)) and (s[i] in ['0'..'9']) do inc(i);
+    Val(Copy(s, j, i-j), v, err);
+    Result := (err = 0) and (v>=min) and (v<=max);
+    if Result then sidx := i;
+  end;
+end;
+
+procedure TECSet.AddWord(aidx, alen: integer);
+begin
+  if Count=length(Strs) then begin
+    if Count=0 then SetLength(Strs,4)
+    else SetLength(Strs,Count*2);
+  end;
+  Strs[Count].offset:=aidx;
+  Strs[Count].len:=alen;
+  inc(Count);
+end;
+
+{ TECContext }
+
+destructor TECContext.Destroy;
+var
+  i: integer;
+begin
+  for i:=0 to count-1 do st[i].Free;
+  inherited Destroy;
+end;
+
+function TECContext.GetSet(const p: string; var pidx: integer): TECSet;
+var
+  i : integer;
+begin
+  for i:=0 to count-1 do
+    if st[i].FullIdx=pidx then begin
+      Result := st[i];
+      pidx := pidx+st[i].FullLen;
+      Exit;
+    end;
+
+  if count=length(st) then begin
+    if count=0 then SetLength(st,4)
+    else SetLength(st, count*2);
+  end;
+  Result := TECSet.Create;
+  Result.Parse(p, pidx);
+  st[count]:=Result;
+  inc(count);
 end;
 
 end.
